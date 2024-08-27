fe6b44e54a0c1a97753e22fae071f40d7f06e9bb
coderHarsh121212 <codesource287@gmail.com>
2024-08-27 14:32:48 +0530

    n

--- static/main.js
+++ static/main.js
@@ -117,7 +117,7 @@ const cancel = () => {
 
 const copy = async() => {
     const roomid = document.getElementById("roomid").innerText;
-    await navigator.clipboard.writeText("https://zoombe.onrender.com/join" + roomid);
+    await navigator.clipboard.writeText("https://zoombe.onrender.com/join/" + roomid);
 };
 const invitebox = () => {
     $("#getCodeModal").modal("show");
